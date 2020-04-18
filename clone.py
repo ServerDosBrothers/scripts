efa5fc1c6cb49f9d896df7460df46cf4efce8c04
arthurdead <arthur.kinder@gmail.com>
2020-04-17 23:51:56 -0300

    suporte pra github e tbm mudei como funciona

--- clone.py
+++ clone.py
@@ -1,31 +1,39 @@
 import os, sys, argparse, requests, json, pathlib, subprocess
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-t", action="store", required=True, dest="toke")
-parser.add_argument("-u", action="store", required=True, dest="user")
-args = parser.parse_args()
+def clone(url, path):
+	old_cwd = os.getcwd()
+	if os.path.exists(path):
+		os.chdir(path)
+		subprocess.run("git fetch origin", shell=True,cwd=os.getcwd())
+		subprocess.run("git reset --hard origin", shell=True,cwd=os.getcwd())
+		subprocess.run("git clean --force -d", shell=True,cwd=os.getcwd())
+		os.chdir(old_cwd)
+	else:
+		os.makedirs(path,exist_ok=True)
+		os.chdir(path)
+		os.system("git clone --recursive "+url+" \"" + path + "\"")
+		os.chdir(old_cwd)
 
-cwd = pathlib.Path(os.getcwd())
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-t", action="store", required=True, dest="toke")
+	parser.add_argument("-u", action="store", required=True, dest="user")
+	args = parser.parse_args()
 
-api_url="https://api.github.com/orgs/ServerDosBrothers/repos?per_page=999"
-with requests.get(api_url, auth=(args.user,args.toke)) as request:
-	json_response = json.loads(request.text)
+	cwd = pathlib.Path(os.getcwd())
 	
-	#print(json.dumps(json_response, indent=4, sort_keys=True))
-	
-	if isinstance(json_response, dict):
-		json_response = [json_response]
-	for repo_entry in json_response:
-		clone_url = repo_entry["clone_url"]
-		if clone_url == None:
-			continue
-		name = repo_entry["name"]
-		path = os.path.join(cwd,name)
-		if os.path.exists(path):
-			os.chdir(path)
-			subprocess.run("git fetch origin", shell=True,cwd=os.getcwd())
-			subprocess.run("git reset --hard origin", shell=True,cwd=os.getcwd())
-			subprocess.run("git clean --force -d", shell=True,cwd=os.getcwd())
-		else:
-			os.chdir(cwd)
-			os.system("git clone --recursive "+clone_url)
+	api_url="https://api.github.com/orgs/ServerDosBrothers/repos?per_page=999"
+	with requests.get(api_url, auth=(args.user,args.toke)) as request:
+		json_response = json.loads(request.text)
+		
+		#print(json.dumps(json_response, indent=4, sort_keys=True))
+		
+		if isinstance(json_response, dict):
+			json_response = [json_response]
+		for repo_entry in json_response:
+			clone_url = repo_entry["clone_url"]
+			if clone_url == None:
+				continue
+			name = repo_entry["name"]
+			path = os.path.join(cwd,name)
+			clone(clone_url, path)
--- package.py
+++ package.py
@@ -1,4 +1,5 @@
 import os, sys, argparse, pathlib, json, subprocess, shutil, distutils.dir_util, requests
+from clone import clone
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", action="store", required=True, dest="sm")
@@ -28,7 +29,6 @@ pak_gamedata = os.path.join(pak_sm,"gamedata")
 base_sp_includes = [
 	sm_scripting,
 	sm_include,
-	os.path.join(cwd,"extra_includes"),
 ]
 
 base_sp_exec = spcomp + " -O2 -v0 -z9"
@@ -66,23 +66,25 @@ def handle_sp_folder(folder, extra_includes, sp_pak_info):
 			includes_str += "-i \"" + str(inc) + "\" "
 		if "SVB-" in gitfolder:
 			file_basename = "svb/" + file_basename
+		else:
+			file_basename = "thirdparty/" + file_basename
+		extra_flags = ""
 		if sp_pak_info:
 			if "name_append" in sp_pak_info:
 				file_basename = sp_pak_info["name_append"] + file_basename
+			if "disable_warnings" in sp_pak_info:
+				for warn in sp_pak_info["disable_warnings"]:
+					extra_flags += "-w" + str(warn) + ' '
+			if "warning_errors" in sp_pak_info:
+				if sp_pak_info["warning_errors"] == True:
+					extra_flags += "-E "
 		output_path = os.path.join(pak_plugins,file_basename+".smx")
 		os.makedirs(pathlib.Path(output_path).parent, exist_ok=True)
-		exec = base_sp_exec + " \"" + str(newfile_path) + "\" -o \"" + output_path + "\" " + includes_str
+		exec = base_sp_exec + " \"" + str(newfile_path) + "\" -o \"" + output_path + "\" " + includes_str + extra_flags
 		exec += defines
 		subprocess.run(exec, shell=True,cwd=os.getcwd())
 		os.remove(newfile_path)
 		print("")
-
-def handle_sp_includes(name, extra_includes):
-	folder = pathlib.Path(os.path.join(cwd,name))
-	for extra in folder.glob("addons/sourcemod/scripting/*"):
-		if extra.is_file():
-			continue
-		extra_includes.append(str(extra))
 
 def copy_folder(src, dst):
 	if os.path.exists(src):
@@ -98,17 +100,52 @@ def copy_folder(src, dst):
 			#shutil.copytree(str(folder),newdst,dirs_exist_ok=True)
 			distutils.dir_util.copy_tree(str(folder),newdst)
 
-def handle_wget(url):
-	extra_includes = os.path.join(cwd,"extra_includes")
-	file_path = os.path.join(extra_includes,os.path.basename(url))
+def handle_git_repo(dep, dep_path, extra_includes):
+	if "addons/sourcemod" in dep:
+		extra_includes += [os.path.join(dep["addons/sourcemod"], "include")]
+
+def handle_wget(url, folder):
+	file_path = os.path.join(folder,os.path.basename(url))
 	if not os.path.exists(file_path):
 		with requests.get(url) as file_request:
-			os.makedirs(extra_includes,exist_ok=True)
+			os.makedirs(folder,exist_ok=True)
 			with open(file_path,"wb+") as file:
 				file.write(file_request.content)
 
+def handle_depends(depends, extra_includes, compile_later):
+	for depend in depends:
+		is_root_repo = True
+		if sp_pak_type:
+			if depend in sp_pak_type:
+				dep = sp_pak_type[depend]
+				type = dep["type"]
+				dep_path = os.path.join(thirdparty,depend)
+				is_root_repo = False
+				if type == "wget":
+					handle_wget(dep["url"], dep_path)
+					extra_includes += [dep_path]
+				elif type == "git":
+					clone(dep["url"], dep_path)
+					if "path_map" in dep:
+						path_map = dep["path_map"]
+						if "addons/sourcemod/scripting/include" in path_map:
+							extra_includes += [os.path.join(dep_path, path_map["addons/sourcemod/scripting/include"])]
+					compile_later += [(dep,dep_path)]
+		if is_root_repo:
+			extra_includes += [os.path.join(cwd,depend,"addons/sourcemod/scripting/include")]
+
+thirdparty = os.path.join(cwd,"thirdparty")
+os.makedirs(thirdparty,exist_ok=True)
 os.makedirs(os.path.join(cwd,"tmp"),exist_ok=True)
 shutil.rmtree(pak,ignore_errors=True)
+
+sp_pak_type = None
+sp_pak_type_path = os.path.join(cwd,"scripts/.sp_pak_type")
+if os.path.exists(sp_pak_type_path):
+	with open(sp_pak_type_path,"r") as file:
+		sp_pak_type = json.load(file)
+
+compile_later  =[]
 
 for folder in cwd.glob("*"):
 	if folder.is_file():
@@ -130,13 +167,41 @@ for folder in cwd.glob("*"):
 			with open(sp_pak_info_path,"r") as file:
 				sp_pak_info = json.load(file)
 				if "depends" in sp_pak_info:
-					for depend in sp_pak_info["depends"]:
-						if "http" not in depend:
-							handle_sp_includes(depend, extra_includes)
-						else:
-							handle_wget(depend)
+					handle_depends(sp_pak_info["depends"], extra_includes, compile_later)
 		
 		handle_sp_folder(os.path.join(addons,"sourcemod/scripting"), extra_includes, sp_pak_info)
+	
+def handle_compile_later(compile_later):
+	compile_later2 = []
+	for dep, dep_path in compile_later:
+		extra_includes = []
+		sp_pak_info = None
+		
+		os.chdir(dep_path)
+		
+		path_map = None
+		if "path_map" in dep:
+			path_map = dep["path_map"]
+		
+		if path_map:
+			if "copy" in path_map:
+				for key, value in path_map["copy"].items():
+					folder = os.path.join(dep_path,key)
+					target = os.path.join(pak,value)
+					copy_folder(folder, target)
+		
+		if "sp_pak_info" in dep:
+			sp_pak_info = dep["sp_pak_info"]
+			if "depends" in sp_pak_info:
+				handle_depends(sp_pak_info["depends"], extra_includes, compile_later2)
+				
+		if path_map:
+			if "addons/sourcemod/scripting" in path_map:
+				handle_sp_folder(os.path.join(dep_path,path_map["addons/sourcemod/scripting"]), extra_includes, sp_pak_info)
+	if compile_later2:
+		handle_compile_later(compile_later2)
+	
+handle_compile_later(compile_later)
 	
 shutil.rmtree(os.path.join(cwd,"tmp"),ignore_errors=True)
 
